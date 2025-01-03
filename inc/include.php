a491e60b18cf2c277e7beb00caae4e0179a940a1
alex-an-der <github@nudelpizza.de>
2025-01-03 15:01:11 +0100

    array2table.php und Referenzierung gelöscht

--- inc/include.php
+++ inc/include.php
@@ -2,8 +2,7 @@
 require_once(__DIR__."/../config/config.php");
 require_once(__DIR__."/classes/datenbank.php");
 $db = new Datenbank();
-/*require_once(__DIR__."/classes/array2table.php");
-$a2t = new Array2Table();*/
+
 
 echo ("<link href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css' rel='stylesheet'>");
 
