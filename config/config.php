9ce1c4105fa15fd573f1b1142302b25fb4a66a3d
alex-an-der <github@nudelpizza.de>
2025-01-13 13:21:24 +0100

    OK soweit, Next: CSV-Import (FK-Spalten im Kopf, nur Input-Daten im Feld)

--- config/config.php
+++ config/config.php
@@ -88,7 +88,16 @@ $anzuzeigendeDaten[] = array(
     "tabellenname" => "b_bsg",
     "auswahltext" => "BSG im Regionalverband",
     "hinweis" => "<b>RE </b> = Rechnungsempfänger. In diese Spalten bitte eintragen, wohin eventuelle Rechnungen geschickt werden sollen.",
-    "query" => "SELECT b.id as id, b.Verband as Verband, b.BSG as BSG
+    "query" => "SELECT 
+        b.id as id,
+        b.Verband as Verband,
+        b.BSG as BSG,
+        Ansprechpartner,
+        RE_Name,
+        RE_Name2,
+        RE_Strasse_Nr,
+        RE_Strasse2,
+        RE_PLZ_Ort
         FROM b_bsg as b
         LEFT JOIN b_regionalverband_rechte as r on r.Verband = b.Verband
         WHERE b.Verband  IS NULL OR Nutzer = $uid
@@ -100,10 +109,16 @@ $anzuzeigendeDaten[] = array(
         join b_regionalverband_rechte as r on r.Verband = v.id 
         where r.Nutzer = $uid 
         ORDER BY anzeige;
-        "
+        ",
+        "Ansprechpartner" => "SELECT m.id, CONCAT(Nachname, ', ', Vorname) as anzeige 
+                                from b_mitglieder as m
+                                join b_bsg as b on b.id=m.BSG
+                                join b_regionalverband_rechte as vr on b.Verband = vr.Verband
+                                where vr.Nutzer = $uid
+                                order by anzeige;"
     ),
     "spaltenbreiten" => array(
-        "Verband"                       => "400",
+        "Verband"                       => "200",
         "BSG"                           => "200",  
         "Ansprechpartner"               => "400",  
         "RE_Name"                       => "200",  
@@ -153,7 +168,30 @@ $anzuzeigendeDaten[] = array(
         "
     )
 );
-
+# Mitglieder in den Sparten 
+$anzuzeigendeDaten[] = array(
+    "tabellenname" => "b_mitglieder_in_sparten",
+    "auswahltext" => "Mitglieder in den Sparten",
+    "query" => "SELECT mis.id as id, mis.Sparte as Sparte, mis.Mitglied as Mitglied
+                from b_mitglieder_in_sparten as mis
+                join v_verbands_berechtigte_sparte as vbs on vbs.Sparte = mis.Sparte
+                where vbs.Verbandsberechtigter = $uid 
+                order by mis.id desc;
+    ",
+    "referenzqueries" => array(
+        "Sparte" => "SELECT Sparte as id, Sparte_Name as anzeige
+                    from v_verbands_berechtigte_sparte
+                    where Verbandsberechtigter = $uid
+                    ORDER BY anzeige;
+        ",
+        "Mitglied" => "SELECT m.id as id, CONCAT(m.Nachname, ', ', m.Vorname, ' (', vbr.BSG_Name,')') as anzeige 
+                        from b_mitglieder as m
+                        join v_verbands_berechtigte_bsg as vbr on m.BSG = vbr.BSG
+                        where vbr.Verbandsberechtigter = $uid
+                        ORDER BY anzeige;
+        "
+    )
+);
 
 
 ###################################################################################
