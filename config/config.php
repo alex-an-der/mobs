2f59ca154bdfab4dbd6fe3b219a53e1131950a75
alex-an-der <github@nudelpizza.de>
2025-01-21 19:46:10 +0100

    v1.0

--- config/config.php
+++ config/config.php
@@ -1,11 +1,11 @@
 <?php 
 require_once(__DIR__ . "/../mods/all.head.php");
 require_once(__DIR__ . "/../mods/config.head.php");
-
+/*
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-
+*/
 # Datenbank-Zugangsdaten
 define("DB_NAME", "db_441127_14");
 define("DB_HOST", "x96.lima-db.de");
