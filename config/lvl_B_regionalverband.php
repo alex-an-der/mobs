87535e21dbf25f3a965dc64d45699c11ff9a1d91
alex-an-der <github@nudelpizza.de>
2025-01-21 23:00:00 +0100

    Kleine Änderungen der Reihenfolge

--- config/lvl_B_regionalverband.php
+++ config/lvl_B_regionalverband.php
@@ -2,8 +2,8 @@
 
 
 # Sparten im Regionalverband
-# Mitglieder in den Sparten 
 # BSG im Regionalverband
+# Mitglieder in den Sparten 
 # Rechteverwaltung: BSG
 
 # Statistik: Mitglieder in Sparten
@@ -30,40 +30,6 @@ $anzuzeigendeDaten[] = array(
         ORDER BY anzeige;
         ",
         "Sportart" => "SELECT id, CONCAT (Sportart,' (',Sportart_Nr,')') as anzeige from b___sportart ORDER BY anzeige;"
-    )
-);
-
-
-
-# Mitglieder in den Sparten 
-$anzuzeigendeDaten[] = array(
-    "tabellenname" => "b_mitglieder_in_sparten",
-    "auswahltext" => "Mitglieder in den Sparten",
-    "writeaccess" => true,
-    "query" => "SELECT mis.id as id, mis.Sparte as Sparte, mis.Mitglied as Mitglied
-                from b_mitglieder_in_sparten as mis
-                left join v_verbands_berechtigte_sparte as vbs on vbs.Sparte = mis.Sparte
-                where vbs.Verbandsberechtigter = $uid or mis.Sparte is NULL 
-                order by mis.id desc;
-    ",
-    "referenzqueries" => array(
-        "Sparte" => "SELECT Sparte as id, Sparte_Name as anzeige
-                    from v_verbands_berechtigte_sparte
-                    where Verbandsberechtigter = $uid
-                    ORDER BY anzeige;
-        ",
-        "Mitglied" => "SELECT m.id as id, CONCAT(m.Nachname, ', ', m.Vorname, ' (', vbr.BSG_Name,')') as anzeige 
-                        from b_mitglieder as m
-                        join v_verbands_berechtigte_bsg as vbr on m.BSG = vbr.BSG
-                        where vbr.Verbandsberechtigter = $uid
-                        ORDER BY anzeige;
-        "
-    ),
-    "suchqueries" => array(
-        "Sparte" => "SELECT Sparte as id, Sparte_Name
-                    from v_verbands_berechtigte_sparte
-                    where Verbandsberechtigter = $uid;",
-        "Mitglied" => "SELECT id, Vorname, Nachname, Mail from b_mitglieder as m join v_verbands_berechtigte_bsg as vbr on m.BSG = vbr.BSG where vbr.Verbandsberechtigter = $uid;"
     )
 );
 
@@ -113,6 +79,38 @@ $anzuzeigendeDaten[] = array(
         "RE_Strasse2"                   => "200",  
         "RE_PLZ_Ort"                    => "200"
     ) 
+);
+
+# Mitglieder in den Sparten 
+$anzuzeigendeDaten[] = array(
+    "tabellenname" => "b_mitglieder_in_sparten",
+    "auswahltext" => "Mitglieder in den Sparten",
+    "writeaccess" => true,
+    "query" => "SELECT mis.id as id, mis.Sparte as Sparte, mis.Mitglied as Mitglied
+                from b_mitglieder_in_sparten as mis
+                left join v_verbands_berechtigte_sparte as vbs on vbs.Sparte = mis.Sparte
+                where vbs.Verbandsberechtigter = $uid or mis.Sparte is NULL 
+                order by mis.id desc;
+    ",
+    "referenzqueries" => array(
+        "Sparte" => "SELECT Sparte as id, Sparte_Name as anzeige
+                    from v_verbands_berechtigte_sparte
+                    where Verbandsberechtigter = $uid
+                    ORDER BY anzeige;
+        ",
+        "Mitglied" => "SELECT m.id as id, CONCAT(m.Nachname, ', ', m.Vorname, ' (', vbr.BSG_Name,')') as anzeige 
+                        from b_mitglieder as m
+                        join v_verbands_berechtigte_bsg as vbr on m.BSG = vbr.BSG
+                        where vbr.Verbandsberechtigter = $uid
+                        ORDER BY anzeige;
+        "
+    ),
+    "suchqueries" => array(
+        "Sparte" => "SELECT Sparte as id, Sparte_Name
+                    from v_verbands_berechtigte_sparte
+                    where Verbandsberechtigter = $uid;",
+        "Mitglied" => "SELECT id, Vorname, Nachname, Mail from b_mitglieder as m join v_verbands_berechtigte_bsg as vbr on m.BSG = vbr.BSG where vbr.Verbandsberechtigter = $uid;"
+    )
 );
 
 
