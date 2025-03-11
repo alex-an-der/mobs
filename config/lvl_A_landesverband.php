37ac7cc03ccc8f2d514ac32949fee2ba7f846cbd
alex-an-der <github@nudelpizza.de>
2025-03-11 12:14:58 +0100

    Log und Rollback nur für Adminrolle sichtbar

--- config/lvl_A_landesverband.php
+++ config/lvl_A_landesverband.php
@@ -44,33 +44,4 @@ $anzuzeigendeDaten[] = array(
 );
 
 
-$anzuzeigendeDaten['log'] = array(
-    "tabellenname" => "log",
-    "auswahltext" => "Log (zur Entwicklung)",
-    "query" => "SELECT  id, id as Nr, zeit as Timestamp, eintrag as Log from log order by zeit desc;",
-    "spaltenbreiten" => array(
-        "Nr"        => "80",
-        "Timestamp" => "220",
-        "Log"       => "1620"
-    )
-);
-$anzuzeigendeDaten['rollback'] = array(
-    "tabellenname" => "log",
-    "auswahltext" => "Rollback",
-    "hinweis" => "Das Löschen und Anlegen neuer Mitglieder ist nicht im Rollback enthalten. Dies geschieht über den Berechtigungsmanager. Über die normale Schnittstelle sollten keine Mitglieder gelöscht werden können.",
-    "query" => "SELECT  id, id as Nr, zeit as Timestamp, autor as Autor, eintrag as Query 
-                from `rollback` order by zeit desc;",
-    "spaltenbreiten" => array(
-        "Nr"        => "80",
-        "Timestamp" => "220",
-        "Autor"     => "220",
-        "Log"       => "600"
-    )
-);
-
-
-
-
-
-
 ?>
