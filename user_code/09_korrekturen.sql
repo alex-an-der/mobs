63ad4498e989c2fdb4798d5c686afd860f3bdecc
alex-an-der <github@nudelpizza.de>
2025-03-24 16:02:59 +0100

    Issue-Tabelle erweitert - in prod-DB bereits umgesetzt.

--- user_code/09_korrekturen.sql
+++ user_code/09_korrekturen.sql
@@ -1,3 +1,5 @@
 ALTER TABLE `b_mitglieder_in_sparten` ADD CONSTRAINT `Mitglied_in_Sparte` UNIQUE (`Sparte`, `Mitglied`);
 ALTER TABLE `b_bsg` CHANGE COLUMN `VKZ` `VKZ` VARCHAR(8) NULL;
 ALTER TABLE `issues` ADD  `version` VARCHAR(50) NULL;
+ALTER TABLE `issues` CHANGE COLUMN `Kommentar` `Kommentar` VARCHAR(2000) NULL;
+ALTER TABLE `issues` CHANGE COLUMN `Issue` `Issue` VARCHAR(2000) NULL;
