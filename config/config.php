f677780409f958ebffe1a8e21083b4d97245025f
alex-an-der <github@nudelpizza.de>
2025-03-24 16:43:11 +0100

    Namesänderung udanma -> mobs. Zudem (wichtiger): delete of db_connect.php und move outside docRoot. Neuverlinkung in config.php

--- config/config.php
+++ config/config.php
@@ -18,7 +18,7 @@ ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 }
 
-require_once("db_connect.php");
+require_once(__DIR__."/../../mobs_credentials/db_connect.php");
 
 define("TITEL", "LBSV Nds. Mitgliederverwaltung");
 # Wie sollen NULL-Werte (=keine Zuordnung) dargestellt werden?
