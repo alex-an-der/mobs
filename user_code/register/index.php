bfbb7a831f709bc6e182e9786a2d481ec1f933f0
alex-an-der <github@nudelpizza.de>
2025-04-01 22:16:40 +0200

    Bugfix - Registerumleitung muss mit absoluter Adresse geschehen wegen Subdoman-Wechsel.

--- user_code/register/index.php
+++ user_code/register/index.php
@@ -1,6 +1,7 @@
 <?php
 // Permanent redirect to ./../ypum/yfront/register.php
 header("HTTP/1.1 301 Moved Permanently");
-header("Location: ./../../ypum/yfront/register.php");
+// Absolute Adresse, wegen Subdomain-Wechsel (register.mobs24.de => www.mobs24.de)
+header("Location: https://www.mobs24.de/ypum/yfront/register.php");
 exit();
 ?>
