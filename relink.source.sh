d35c646df732ead9e4561694d367946370fb2962
alex-an-der <github@nudelpizza.de>
2025-04-19 21:19:53 +0200

    relink - Skript erstellt (für login und register)

---
+++ relink.source.sh
@@ -0,0 +1,9 @@
+rm ypum/yfront/login.php
+ln ./user_code/login.php ./ypum/yfront/login.php
+rm ypum/yfront/register.php
+ln ./user_code/register.php ./ypum/yfront/register.php
+ll -i ypum/yfront/login.php
+ll -i user_code/login.php
+
+ll -i ypum/yfront/register.php
+ll -i user_code/register.php
