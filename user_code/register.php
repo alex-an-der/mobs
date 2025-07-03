48b8288004d099b6cf65ac6578318b67f0326d73
alex-an-der <github@nudelpizza.de>
2025-07-03 20:57:10 +0200

    Register - Tabname angepasst

--- user_code/register.php
+++ user_code/register.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset='UTF-8'>
 <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-<title>Form-Vorlage</title>
+<title>Registrierung</title>
 <?php 
 require_once(__DIR__.'/../yback/include/inc_main.php');
 require_once(__DIR__."/../../config/db_connect.php");
