8b31912703e649e8bc55cdc7dcca32f31bc01951
alex-an-der <github@nudelpizza.de>
2025-07-15 13:01:00 +0200

    minor bug fixed

--- user_includes/before_logging.php
+++ user_includes/before_logging.php
@@ -2,6 +2,10 @@
 # $pdo ist ein PDO-Objekt für die aktuelle Datenbank
 # $eintrag wird im nächsten Schritt zusammen mit dem aktuellen Timestamp in die Datenbank geschrieben
 global $ypum;
+if ($ypum === null) {
+    // Passiert auch durch Direktaufrufe
+    die("not allowed"); // oder eine alternative Aktion ausführen
+}
 $userData = $ypum->getUserData();
 $userMail = $userData['mail'];
 $eintrag .= " ($userMail)";
